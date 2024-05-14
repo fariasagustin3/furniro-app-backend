77b2fb3b2d541cb39fdfb2cc967255cad5232f09
fariasagustin3 <fariasagustin3@gmail.com>
2024-05-14 15:08:53 -0300

    .

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/auth", authRoute);
-app.use("/ready", (req, res) => {
+app.get("/ready", (req, res) => {
   res.status(200).json({ message: "OK "})
 })
 
