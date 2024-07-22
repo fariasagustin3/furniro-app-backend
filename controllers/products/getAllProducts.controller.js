0c21171563894d05602e048579248dbb7a38c907
fariasagustin3 <fariasagustin3@gmail.com>
2024-07-22 15:42:48 -0300

    get all products arreglado

--- controllers/products/getAllProducts.controller.js
+++ controllers/products/getAllProducts.controller.js
@@ -1,31 +1,43 @@
 const Product = require("../../models/Product");
 
 const getAllProducts = async (req, res) => {
-  const page = parseInt(req.query.page)
-  const limit = parseInt(req.query.limit)
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
   const skip = (page - 1) * limit;
-  const sorted = parseInt(req.query.sort);
-  const filter = req?.query?.filter;
-  const search = req?.query?.search;
+  const sort = parseInt(req.query.sort) || -1; // Default to descending order
+  const filter = req.query.filter;
+  const search = req.query.search;
+
+  const query = {};
+
+  if (filter) {
+    query.categoryName = filter;
+  }
+
+  if (search) {
+    query.title = { $regex: search, $options: "i" };
+  }
 
   try {
-    console.log(filter, search)
-    
-    if(filter) {
-      const totalProducts = await Product.countDocuments({ categoryName: filter, title: { $regex: req?.query?.search, $options: "i" } });
-      const totalPages = Math.ceil(totalProducts / limit)
-      const products = await Product.find({ categoryName: filter, title: { $regex: search, $options: "i" } }).skip(skip).limit(limit).sort({ price: sorted });
-      return res.status(200).json({ products, page, limit, totalPages, totalProducts })
-    } else {
-      const totalProducts = await Product.countDocuments({ title: { $regex: req?.query?.search, $options: "i" } });
-      const totalPages = Math.ceil(totalProducts / limit)
-      const products = await Product.find({ title: { $regex: search, $options: "i" } }).skip(skip).limit(limit).sort({ price: sorted });
-      return res.status(200).json({ products, page, limit, totalPages, totalProducts })
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-}
+    const totalProducts = await Product.countDocuments(query);
+    const totalPages = Math.ceil(totalProducts / limit);
 
-module.exports = { getAllProducts }
+    const products = await Product.find(query)
+      .skip(skip)
+      .limit(limit)
+      .sort({ price: sort });
+
+    return res.status(200).json({
+      products,
+      page,
+      limit,
+      totalPages,
+      totalProducts,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { getAllProducts };
